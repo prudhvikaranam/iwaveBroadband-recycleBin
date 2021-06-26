d0241281cd1b6768dcdde70f1b6eacd564315abe
PrudhviKaranam <prudhvikaranam537@gmail.com>
2021-06-26 18:27:42 +0530

    adding contact section

--- script.js
+++
@@ -1,6 +0,0 @@
-function initMap() {
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 8,
-      center: { lat: 17.438336906533447, lng: 78.55750717865895 },
-    });
-  }
