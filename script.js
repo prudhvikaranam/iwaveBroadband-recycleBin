fbe8aabc62d480a7645f1b2ed75556a411ad5c17
PrudhviKaranam <prudhvikaranam537@gmail.com>
2021-06-27 22:21:48 +0530

    adding automatic slider in lander page

---
+++ script.js
@@ -0,0 +1,18 @@
+let ele = Array.from(document.getElementsByClassName("slider"));
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("lander-hero");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+  x[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 4000); 
+}
