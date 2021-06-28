663391bd96b2d562398876cae4c3a1db5fcad5ad
PrudhviKaranam <prudhvikaranam537@gmail.com>
2021-06-28 15:32:33 +0530

    adding fade in effects

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Autoplay slider
 // var myIndex = 0;
 // carousel();
 
@@ -12,30 +13,69 @@
 //     myIndex = 1;
 //   }
 //   x[myIndex - 1].style.display = "block";
-//   setTimeout(carousel, 10000); 
+//   setTimeout(carousel, 10000);
 // }
 
-
-
+// Manual slider
 var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-  showDivs(slideIndex += n);
+  showDivs((slideIndex += n));
 }
 
 function currentDiv(n) {
-  showDivs(slideIndex = n);
+  showDivs((slideIndex = n));
 }
 
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("lander-hero");
   var dots = document.getElementsByClassName("slider-selector");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+  if (n > x.length) {
+    slideIndex = 1;
   }
-  x[slideIndex-1].style.display = "block";  
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
 }
+
+
+
+// slide shows
+
+const items = document.querySelectorAll("#body-container > div");
+
+console.log(items);
+
+const isInViewport = (el) => {
+  const rect = el.getBoundingClientRect();
+  console.log(el.classList[0]);
+
+//   console.log(el.classList.indexOf('aboutus'));
+  if(el.classList[0] == 'aboutus'){
+    console.log(rect);
+  }
+
+  return (
+    rect.top <= 120 &&
+    rect.left >= 0 &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const run = () =>
+  items.forEach(item => {
+    if (isInViewport(item)) {
+      item.classList.add('elementActivated');
+    }
+  });
+
+// Events
+window.addEventListener('load', run);
+window.addEventListener('resize', run);
+window.addEventListener("scroll", run);
